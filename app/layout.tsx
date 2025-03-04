e523c0c35c64d8a54ee154506aa1b942d2c54334
Lucadev06 <francosimonazzi@hotmail.com>
2025-03-04 12:23:47 -0300

    fix/ navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
-        <Navbar />
         <main>{children}</main>
       </body>
     </html>
