09f006d250425e97f9e7ede9068586256ebc46e7
Luca <lucasimodev01@gmail.com>
2025-04-03 19:57:15 -0300

    ultimo fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,61 +3,16 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-export const metadata = {
-  title: "Hasser Motor Works",
-  description:
-    "Especialistas en inyecciones, modificaciones y swaps. Hasser Motor Works ofrece soluciones personalizadas para autos clásicos y modernos.",
-  openGraph: {
-    title: "Hasser Motor Works",
-    description: "Especialistas en inyecciones, modificaciones y swaps.",
-    url: "https://hassermotorworks.com",
-    siteName: "Hasser Motor Works",
-    images: [
-      {
-        url: "https://hassermotorworks.com/images/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Hasser Motor Works",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Hasser Motor Works",
-    description: "Especialistas en inyecciones, modificaciones y swaps.",
-    images: ["https://hassermotorworks.com/images/og-image.jpg"],
-  },
-  alternates: {
-    canonical: "https://hassermotorworks.com",
-  },
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Hasser Motor Works',
+  description: 'Especialistas en inyecciones, modificaciones y swaps. Hasser Motor Works ofrece soluciones personalizadas para autos clásicos y modernos. Con un equipo de expertos, garantizamos calidad y rendimiento en cada proyecto.'
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: `
-                    {
-          "@context": "https://schema.org",
-          "@type": "WebSite",
-          "name": "Hasser Motor Works",
-          "alternateName": ["HMW", "hassermotorworks.com"],
-          "url": "https://hassermotorworks.com"
-        }
-      `,
-            }}
-          />
-        <title>Hasser Motor Works</title>
-      </head>
-
       <body>{children}</body>
     </html>
   );
