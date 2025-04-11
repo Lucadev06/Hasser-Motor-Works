34ecb846d96710c9532fe0dfb94e089f0479ba97
Luca <lucasimodev01@gmail.com>
2025-04-11 13:00:16 -0300

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,25 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Hasser Motor Works',
-  description: 'Especialistas en inyecciones, modificaciones y swaps. Hasser Motor Works ofrece soluciones personalizadas para autos clásicos y modernos. Con un equipo de expertos, garantizamos calidad y rendimiento en cada proyecto.'
+  title: "Hasser Motor Works",
+  description:
+    "Especialistas en inyecciones, modificaciones y swaps. Hasser Motor Works ofrece soluciones personalizadas para autos clásicos y modernos. Con un equipo de expertos, garantizamos calidad y rendimiento en cada proyecto.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="es">
+      <head>
+        <link rel="canonical" href="https://hassermotorworks.com/" />
+      </head>
+
       <body>{children}</body>
     </html>
   );
